fb210230055d187f46ab23c13710b2195146b700
Kelyn Rodríguez <90913597+Kelyn24@users.noreply.github.com>
2022-07-05 17:32:34 -0500

    Lookups Kelyn

--- database/seeders/LookupSeeder.php
+++ database/seeders/LookupSeeder.php
@@ -18,5 +18,24 @@ class LookupSeeder extends Seeder
         Lookup::create(['type' => TypeLookup::StatusUser->value, 'name' => 'Activo']);
         Lookup::create(['type' => TypeLookup::StatusUser->value, 'name' => 'Inactivo']);
         Lookup::create(['type' => TypeLookup::StatusUser->value, 'name' => 'Bloqueado']);
+        
+        Lookup::create(['type' => TypeLookup::ServicesList->value, 'name' => 'Sala de Juntas']);
+        Lookup::create(['type' => TypeLookup::ServicesList->value, 'name' => 'Automóvil']);
+        Lookup::create(['type' => TypeLookup::ServicesList->value, 'name' => 'Conductor']);
+
+        Lookup::create(['type' => TypeLookup::StatusRequest->value, 'name' => 'Nueva']);
+        Lookup::create(['type' => TypeLookup::StatusRequest->value, 'name' => 'Aprobada']);
+        Lookup::create(['type' => TypeLookup::StatusRequest->value, 'name' => 'Rechazada']);
+        Lookup::create(['type' => TypeLookup::StatusRequest->value, 'name' => 'Propuesta']);
+        Lookup::create(['type' => TypeLookup::StatusRequest->value, 'name' => 'Cancelada']);
+        Lookup::create(['type' => TypeLookup::StatusRequest->value, 'name' => 'Sin asistir']);
+
+        Lookup::create(['type' => TypeLookup::LevelMeeting->value, 'name' => 'Administrativa']);
+        Lookup::create(['type' => TypeLookup::LevelMeeting->value, 'name' => 'Directiva']);
+
+        Lookup::create(['type' => TypeLookup::TypeSnack->value, 'name' => 'Bebida']);
+        Lookup::create(['type' => TypeLookup::TypeSnack->value, 'name' => 'Snack']);
+        Lookup::create(['type' => TypeLookup::TypeSnack->value, 'name' => 'Bocadillo']);
+
     }
 }
