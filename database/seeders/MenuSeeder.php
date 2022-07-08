d61227dadb10ed7c1b8f73387bcd186e92e209e4
jcoronelg <programador.analista24@ciudadmaderas.com>
2022-07-08 09:20:42 -0500

    Seeder de menu y submenu * Se agregan 3 registros de menú (temporales) para pruebas * Se crea SubmenuSeeder.php * Se agregan 6 registros de submenu (temporales) para pruebas * Se registra SubmenuSeeder en el DatabaseSeeder

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -20,5 +20,26 @@ class MenuSeeder extends Seeder
             'icon' => 'mat:insights',
             'order' => 1
         ]);
+
+        Menu::create([
+            'path_route' => '/dashboard/solicitud',
+            'label' => 'Solicitudes',
+            'icon' => 'mat:note_add',
+            'order' => 2
+        ]);
+
+        Menu::create([
+            'path_route' => '/dashboard/historial',
+            'label' => 'Historial',
+            'icon' => 'mat:history',
+            'order' => 3
+        ]);
+
+        Menu::create([
+            'path_route' => '/dashboard/calendario',
+            'label' => 'Calendario',
+            'icon' => 'mat:calendar_month',
+            'order' => 4
+        ]);
     }
 }
