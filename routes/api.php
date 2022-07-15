82682fa38535496d5ed6b708cbf77a9b2f427c33
jcoronelg <programador.analista24@ciudadmaderas.com>
2022-07-15 09:09:06 -0500

    Se agrega resource de usuario controller pero solo el método store

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
@@ -11,6 +12,9 @@ Route::prefix('v1')->group(function () {
             Route::post('/login', 'login')->name('login');
             Route::post('/restore-password', 'restorePassword')->name('restore.password');
         });
+
+    Route::apiResource('users', UserController::class)
+        ->only(['store']);
 
     // Rutas con autenticación
     Route::group(['middleware' => 'auth:sanctum'], function () {
