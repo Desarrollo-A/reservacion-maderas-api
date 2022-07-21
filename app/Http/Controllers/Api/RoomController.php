747efdfb1f64e84d918a2cdfec8d1e722c6fe5e7
jcoronelg <programador.analista24@ciudadmaderas.com>
2022-07-21 10:53:17 -0500

    Controlador de sala * Se agrega función para actualizar el estatus de sala * Se agrega validación si un lookup es válido

--- app/Http/Controllers/Api/RoomController.php
+++ app/Http/Controllers/Api/RoomController.php
@@ -2,27 +2,30 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Contracts\Services\ILookupService;
 use App\Contracts\Services\IRoomService;
 use App\Core\BaseApiController;
+use App\Http\Requests\Room\ChangeStatusRoomRequest;
 use App\Http\Requests\Room\StoreRoomRequest;
 use App\Http\Resources\Room\RoomResource;
 use App\Models\Enums\NameRole;
+use App\Models\Enums\TypeLookup;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 
 class RoomController extends BaseApiController
 {
     private IRoomService $roomService;
+    private ILookupService $lookupService;
 
-    /**
-     * @param IRoomService $roomService
-     */
-    public function __construct(IRoomService $roomService)
+    public function __construct(IRoomService $roomService, ILookupService $lookupService)
     {
         $this->middleware('role.permission:'.NameRole::ADMIN->value)
             ->only('store');
         $this->middleware('role.permission:'.NameRole::ADMIN->value.','.NameRole::RECEPCIONIST->value)
-            ->only('show');
+            ->only('show', 'changeStatus');
         $this->roomService = $roomService;
+        $this->lookupService = $lookupService;
     }
 
     /**
@@ -39,5 +42,16 @@ class RoomController extends BaseApiController
     {
         $room = $this->roomService->findById($id);
         return $this->showOne(new RoomResource($room));
+    }
+
+    /**
+     * @throws \Spatie\DataTransferObject\Exceptions\UnknownProperties
+     */
+    public function changeStatus(int $id, ChangeStatusRoomRequest $request): Response
+    {
+        $roomDTO = $request->toDTO();
+        $this->lookupService->validateLookup($roomDTO->statusId, TypeLookup::STATUS_ROOM->value, 'Estatus no válido.');
+        $this->roomService->changeStatus($id, $roomDTO);
+        return $this->noContentResponse();
     }
 }
